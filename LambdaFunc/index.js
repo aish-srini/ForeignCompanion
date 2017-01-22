ea70297baed39111d2bb61a1b09bc5f3cd3a9536
Tian <tianqin@berkeley.edu>
2017-01-21 20:59:45 -0800

    Update index.js

--- LambdaFunc/index.js
+++ LambdaFunc/index.js
@@ -31,13 +31,13 @@ const handlers = {
         }
 
         const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
-        const myRecipes = this.t('RECIPES');
-        const recipe = myRecipes[itemName];
+        const myPrompts = this.t('PROMPTS');
+        const prompt = myPrompts[itemName];
 
-        if (recipe) {
-            this.attributes.speechOutput = recipe;
+        if (prompt) {
+            this.attributes.speechOutput = prompt;
             this.attributes.repromptSpeech = this.t('RECIPE_REPEAT_MESSAGE');
-            this.emit(':askWithCard', recipe, this.attributes.repromptSpeech, cardTitle, recipe);
+            this.emit(':askWithCard', prompt, this.attributes.repromptSpeech, cardTitle, prompt);
         } else {
             let speechOutput = this.t('RECIPE_NOT_FOUND_MESSAGE');
             const repromptSpeech = this.t('RECIPE_NOT_FOUND_REPROMPT');
@@ -76,7 +76,7 @@ const handlers = {
 const languageStrings = {
     'en-GB': {
         translation: {
-            RECIPES: recipes.RECIPE_EN_GB,
+            PROMPTS: prompts.PROMPTS_IN_ENGLISH,
             SKILL_NAME: 'British Minecraft Helper',
             WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
             WELCOME_REPROMT: 'For instructions on what you can say, please say help me.',
@@ -93,7 +93,7 @@ const languageStrings = {
     },
     'en-US': {
         translation: {
-            RECIPES: recipes.RECIPE_EN_US,
+            PROMPTS: prompts.PROMPTS_IN_ENGLISH,
             SKILL_NAME: 'American Minecraft Helper',
             WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
             WELCOME_REPROMT: 'For instructions on what you can say, please say help me.',
@@ -106,23 +106,6 @@ const languageStrings = {
             RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
             RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
             RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
-        },
-    },
-    'de-DE': {
-        translation: {
-            RECIPES: recipes.RECIPE_DE_DE,
-            SKILL_NAME: 'Assistent für Minecraft in Deutsch',
-            WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?',
-            WELCOME_REPROMT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
-            DISPLAY_CARD_TITLE: '%s - Rezept für %s.',
-            HELP_MESSAGE: 'Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            HELP_REPROMT: 'Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-            STOP_MESSAGE: 'Auf Wiedersehen!',
-            RECIPE_REPEAT_MESSAGE: 'Sage einfach „Wiederholen“.',
-            RECIPE_NOT_FOUND_MESSAGE: 'Tut mir leid, ich kenne derzeit ',
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'das Rezept für %s nicht. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'dieses Rezept nicht. ',
-            RECIPE_NOT_FOUND_REPROMPT: 'Womit kann ich dir sonst helfen?',
         },
     },
 };
