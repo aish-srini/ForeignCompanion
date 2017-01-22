b600be448b00867fc6e76384c2a088074adbd1e2
aish-srini <ashwarya_srini@berkeley.edu>
2017-01-21 20:36:50 -0800

    Create prompts.js

---
+++ LambdaFunc/prompts.js
@@ -0,0 +1,20 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+module.exports = {
+    PROMPTS_IN_ENGLISH: {
+        'hello': 'hello',
+        'swim': 'swim',
+        'lunch': 'lunch',
+        'potato': 'potato',
+        'name': 'name',
+    }
+
+    // STUFF_IN_SPANISH: {
+    //
+    //   //input translate api Spanish
+    //     'swim': 'nadar',
+    //     'hello': 'hola',
+    //     'lunch': 'almuerzo'
+    //     'potato': 'potato'
+    //     },
+};
